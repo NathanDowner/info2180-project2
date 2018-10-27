8a227d9a94b6aef7089c79d0dd097965a9ed7898
Nathan Downer <nathandowner123@gmail.com>
2018-10-26 20:24:50 -0500

    layed out tiles

---
+++ fifteen.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+  const pieces = document.querySelectorAll('#puzzlearea div');
+  const second = document.querySelector('#puzzlearea div:nth-child(2)');
+  // console.log(pieces);
+
+  //add the puzzlepiece class to divs
+  pieces.forEach(p => p.classList.add('puzzlepiece'));
+  
+  //space them out
+  let p = 0
+  for (let i = 0; i < 400; i+=100) {
+    for (let j = 0; j < 400; j+=100) {
+      pieces[p].style.top = `${i}px`;
+      pieces[p].style.left = `${j}px`;
+      p++;
+    }    
+  }
+
+
+ 
+  // pieces.forEach( piece => {
+  //   let txt = piece.textContent;
+  //   if (txt < 5 ) {
+
+  //   } else if (txt < 9) {
+      
+  //   } else if (txt < 13) {
+
+  //   } else {
+
+  //   }
+  // });
+  // for (j=0; j<5; j++) {
+  //   for (i=0; i<300; i+=100) {
+  //     console.log(j,i);
+  //   }
+  // }
+    
+  
+  
+  
+  //break the picture up
+}
